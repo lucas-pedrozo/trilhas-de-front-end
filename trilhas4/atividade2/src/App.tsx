a22cd4c94f3f69415609cf6b0b3d77f5f3beead7
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-04-13 11:26:38 -0300

    trilha 4 atividade 2

--- trilhas4/atividade2/src/App.tsx
+++ trilhas4/atividade2/src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const [erro, setErro] = useState('');
+
+  const cadastrarUsuario = async () => {
+    setMensagem('');
+    setErro('');
+
+    if (!name || !email) {
+      setErro('Todos os Campos são obrigatórios.');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email })
+      });
+
+      if (!response.ok) {
+        setErro('Erro ao cadastrar usuário');
+        alert('Erro ao cadastrar usuário');
+      }
+
+      const data = await response.json();
+      setMensagem(`Usuário "${data.name}"cadastrado com sucesso!`);
+      alert(`Usuário "${data.name}"cadastrado com sucesso!`);
+      setName('');
+      setEmail('');
+    } catch (error) {
+      console.log(error);
+      alert('Erro ao cadastrar usuário.');
+      setErro('Erro ao cadastrar usuário.');
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Cadastrar Novo Usuário</h1>
+      <section>
+        <form onSubmit={(e) => { e.preventDefault(); cadastrarUsuario(); }}>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Digite o nome do usuário"
+          />
+
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite o email do usuário"
+          />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        {mensagem && <p className="text-green-500 mt-4">{mensagem}</p>}
+        {erro && <p className="text-red-500 mt-4">{erro}</p>}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
